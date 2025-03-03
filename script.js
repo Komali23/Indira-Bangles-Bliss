1d327f8f2e40e5b6cb975cfdaf40f513c1e40edc
Komali23 <87021213+Komali23@users.noreply.github.com>
2025-03-03 10:59:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function openWishlist() { window.location.href = 'wishlist.html'; }
+function openCart() { window.location.href = 'cart.html'; }
+function checkout() { alert('Proceeding to Payment'); }
